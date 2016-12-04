e008aff547b63cbd3a549fac07d8a81d88e29c0d
alex.paclin <alex.paclin@gmail.com>
2016-12-04 21:28:04 +0500

    Работа со сложными моделями

--- Soccer/Controllers/HomeController.cs
+++ Soccer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Soccer.Models;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -27,6 +28,52 @@ namespace Soccer.Controllers
 				return HttpNotFound();
 			}
 			return View(team);
+		}
+
+		[HttpGet]
+		public ActionResult Create()
+		{
+			// Формируем список команд для передачи в представление
+			SelectList teams = new SelectList(db.Teams, "Id", "Name");
+			ViewBag.Teams = teams;
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Player player)
+		{
+			//Добавляем игрока в таблицу
+			db.Players.Add(player);
+			db.SaveChanges();
+			// перенаправляем на главную страницу
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return HttpNotFound();
+			}
+			// Находим в бд футболиста
+			Player player = db.Players.Find(id);
+			if (player != null)
+			{
+				// Создаем список команд для передачи в представление
+				SelectList teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+				ViewBag.Teams = teams;
+				return View(player);
+			}
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Player player)
+		{
+			db.Entry(player).State = EntityState.Modified;
+			db.SaveChanges();
+			return RedirectToAction("Index");
 		}
 	}
 }
